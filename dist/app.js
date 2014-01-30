037498308592d83227d1a4816ffcad36ac6c2253
Christophe Promé <chprome@gmail.com>
2014-01-30 11:43:57 +0100

    Affiche un rectangle

--- dist/app.js
+++ dist/app.js
@@ -1,7 +1,25 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-require('./main');
-},{"./main":2}],2:[function(require,module,exports){
-module.exports = {
+var stage = new Kinetic.Stage({
+  container: 'container',
+  width: 578,
+  height: 200
+});
 
-};
+var layer = new Kinetic.Layer();
+
+var rect = new Kinetic.Rect({
+  x: 239,
+  y: 75,
+  width: 100,
+  height: 50,
+  fill: 'green',
+  stroke: 'black',
+  strokeWidth: 3
+});
+
+// add the shape to the layer
+layer.add(rect);
+
+// add the layer to the stage
+stage.add(layer);
 },{}]},{},[1])
--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,23 @@
-require('./main');
+var stage = new Kinetic.Stage({
+  container: 'container',
+  width: 578,
+  height: 200
+});
+
+var layer = new Kinetic.Layer();
+
+var rect = new Kinetic.Rect({
+  x: 239,
+  y: 75,
+  width: 100,
+  height: 50,
+  fill: 'green',
+  stroke: 'black',
+  strokeWidth: 3
+});
+
+// add the shape to the layer
+layer.add(rect);
+
+// add the layer to the stage
+stage.add(layer);
